e7cdd82a4700ca9039462c84e2a884ff088938f9
Vincent Dubuis <vincent.dubuis@heig-vd.ch>
2023-06-01 10:45:59 +0200

    correction checkbox pour les notification

--- laravel/resources/js/components/TheNotification.vue
+++ laravel/resources/js/components/TheNotification.vue
@@ -1,8 +1,8 @@
 <template>
     <button id="cancel" @click="$emit('close')"><img id="cancelcross" src="../../assets/cancel.png"/></button>
-    <div>Live <Toggle></Toggle></div>
-    <div>Sondage <Toggle></Toggle></div>
-    <div>Appel <Toggle></Toggle></div>
+    <div>Live <input type="checkbox"/></div>
+    <div>Sondage <input type="checkbox"/></div>
+    <div>Appel <input type="checkbox"/></div>
     <button id="cancel" @click="$emit('close')">Annuler</button>
                 <button type="submit">
                     Valider
@@ -36,72 +36,5 @@ import Toggle from './TheToggle.vue';
     #cancelcross{
         width: 20px;
         height: 20px;
-    }
-    #toggle {
-    margin: 0 auto;
-    width: 100px;
-    text-align: center;
-    background-color: lightgray;
-    }
-
-    body {
-    text-align: center;
-    padding: 50px
-    }
-    .switch {
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-    }
-
-    .switch input {
-    display: none;
-    }
-
-    .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-    }
-
-    .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-    }
-
-    input:checked + .slider {
-    background-color: #101010;
-    }
-
-    input:focus + .slider {
-    box-shadow: 0 0 1px #101010;
-    }
-
-    input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-    }
-
-    .slider.round {
-    border-radius: 34px;
-    }
-
-    .slider.round:before {
-    border-radius: 50%;
     }
 </style>

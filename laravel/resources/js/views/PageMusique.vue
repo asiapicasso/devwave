9db4e530937f96118e891365670d8edcbe64217f
Marine Hosch <marine.hosch@gmail.com>
2023-06-01 14:29:59 +0200

    up and downvote fonctionnel sans css

--- laravel/resources/js/views/PageMusique.vue
+++ laravel/resources/js/views/PageMusique.vue
@@ -1,15 +1,47 @@
-<script>
-import BaseButton from '../components/BaseButton.vue';
-import { defineProps } from 'vue';
-export default{
-    name : "PageMusique",
-    components: {
-    BaseButton,
-    }
-}
-
-</script>
 <template>
-    <h1>Pageee Musique</h1>
-   <BaseButton />
+    <div>
+        <h1>Page Musique</h1>
+
+        <BaseButton v-for="(item, index) in post" :key="index" :text="item.title" :score="item.score"
+            @upvote="handleUpvote(index)" @downvote="handleDownvote(index)" />
+    </div>
 </template>
+
+<script>
+import { ref } from "vue";
+import BaseButton from "@/components/BaseButton.vue";
+
+export default {
+    name: "PageMusique",
+    components: {
+        BaseButton,
+    },
+    data() {
+        return {
+            post: [
+                {
+                    title: "Devil Dance Floor - Flogging Molly",
+                },
+                {
+                    title: "Woow - La rappresentante di lista",
+                },
+                {
+                    title: "The Pretender - Foo Fighters",
+                },
+            ],
+        };
+    },
+    methods: {
+        handleUpvote(index) {
+            this.post[index].score.value++;
+        },
+        handleDownvote(index) {
+            this.post[index].score.value--;
+        },
+    },
+};
+</script>
+
+<style scoped>
+/* Styles for PageMusique component */
+</style>

45490cb8a60c3e4542bf718b710ee1829648fb7c
Marion de Kerchove <marion.dekerchove@heig-vd.ch>
2023-06-01 15:50:23 +0200

    Migrations terminées

--- laravel/database/migrations/2023_05_31_091536_create_songs_table.php
+++ laravel/database/migrations/2023_05_31_091536_create_songs_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('title');
             $table->integer('year');
             $table->time('duration');
-            $table->foreignId('album_id')->constrained();
             $table->timestamps();
         });
     }

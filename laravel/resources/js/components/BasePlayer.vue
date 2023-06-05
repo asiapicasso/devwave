704cea9d05d64bed0ddcca20919f59bc418fc050
Caroline Hache <hachecaroline@hotmail.com>
2023-06-05 10:49:03 +0200

    Change Player and toggleIcon

--- laravel/resources/js/components/BasePlayer.vue
+++ laravel/resources/js/components/BasePlayer.vue
@@ -1,31 +1,45 @@
 <template>
     <router-link to="/pagePlayer">
-    <div id="player" class=" shadow-md bg-jaune-clair">
-        <div id="info-emission">
-            <p>Nom de l'émission</p>
-            <p>Nom animateurs</p>
+        <div id="player" class="h-20 shadow-md bg-jaune-clair">
+            <div class="ml-5" id="info-emission">
+                <p>Nom de l'émission</p>
+                <p>Nom animateurs</p>
+            </div>
+            <button @click="togglePlayPause">
+                <img :src="playbackIcon" class="h-6 w-6 mr-9" />
+            </button>
         </div>
-        <img id="icon-player" src="../../assets/play.png"/>
-    </div>
-</router-link>
+    </router-link>
 </template>
-<script setup>
+<script>
+import playIcon from "../../assets/Play.svg";
+import pauseIcon from "../../assets/Pause.svg";
+export default {
+    name: "BasePlayer",
+    data() {
+        return {
+            isPlaying: false,
+        };
+    },
+    methods: {
+        togglePlayPause() {
+            this.isPlaying = !this.isPlaying;
+        },
+    },
+    computed: {
+        playbackIcon() {
+            return this.isPlaying ? pauseIcon : playIcon;
+        },
+    },
+};
 </script>
 <style>
-
 #player {
-        text-align: center;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        padding: 16px;
-        text-align: left;
-    }
-
-#icon-player{
-        width: 50px;
-        height: 50px;
-    }
-
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    text-align: left;
+}
 </style>

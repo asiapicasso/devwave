72bc3e6f664871fadac0818f7a98980ea10a625b
Laura DAddese <laura.daddese@heig-vd.ch>
2023-06-06 13:26:40 +0200

    mise en place

---
+++ laravel/resources/js/views/PageLiveVoiture.vue
@@ -0,0 +1,22 @@
+<template>
+    voiture
+</template>
+
+<script>
+export default {
+    name: 'PageLiveVoiture',
+    data() {
+        return {
+            title: 'My Header'
+        };
+    },
+    methods: {
+        launchPhoneApp() {
+            window.open('tel:0840 003 003'); // Remplacez +123456789 par le numéro de téléphone souhaité
+        }
+    }
+};
+</script>
+
+<style>
+</style>
